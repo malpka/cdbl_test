054557ee9157c973fe3619a6635489908b416918
Piotr Piwowarczyk <malpka+githubcom@gmail.com>
2020-09-03 22:46:50 +0200

    Added some integration tests

---
+++ webapi.Tests/EmailControllerTests.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using webapi.Domain;
+using webapi.DTOs;
+using webapi.Models;
+using Xunit;
+
+namespace webapi.Tests
+{
+    [Collection("Integration Tests")]
+    public class EmailControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private Email _modelEmail = null;
+
+        public EmailControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+
+            var dbContext = _factory.Services.GetRequiredService<EmailDbContext>();
+            _modelEmail =  new Email()
+            {
+                Id = 1,
+                Description = "Email Text",
+                Priority = 4,
+                Sender = "sender@gmail.com",
+                Recipients = "recipient@gmail.com",
+                Status = EmailStatus.Pending
+            };
+            dbContext.Emails.Add(_modelEmail);
+            dbContext.SaveChanges();
+        }
+
+        [Fact]
+        public async Task GetRoot_ReturnsSuccessAndStatusUp()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotNull(response.Content);
+            var responseObject = JsonSerializer.Deserialize<ResponseType>(
+                await response.Content.ReadAsStringAsync(),
+                new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
+            Assert.Equal("Up", responseObject?.Status);
+        }
+
+        private class ResponseType
+        {
+            public string Status { get; set; }
+        }
+
+
+        //  [Fact]
+        // public async Task GetEmail_ReturnsSuccessAndEmail()
+        // {
+        //     // Arrange
+        //     var client = _factory.CreateClient();
+
+        //     // Act
+        //     var response = await client.GetAsync("/email/1");
+
+        //     // Assert
+        //     response.EnsureSuccessStatusCode();
+        //     Assert.NotNull(response.Content);
+        //     var responseEmail = JsonSerializer.Deserialize<EmailDTO>(
+        //         await response.Content.ReadAsStringAsync(),
+        //         new JsonSerializerOptions {PropertyNameCaseInsensitive = true});
+        //     Assert.NotNull(responseEmail);
+        //     Assert.NotNull(_modelEmail);
+        //     Assert.Equal(_modelEmail.Id, responseEmail.Id);
+        //     Assert.Equal(_modelEmail.Recipients, responseEmail.Recipients);
+        //     Assert.Equal(_modelEmail.Description, responseEmail.Description);
+        //     Assert.Equal(_modelEmail.Priority, responseEmail.Priority);
+        //     Assert.Equal(_modelEmail.Recipients, responseEmail.Recipients);
+        // }
+    }
+}
